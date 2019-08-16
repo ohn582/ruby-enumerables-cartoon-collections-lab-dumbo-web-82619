92e32ec799853546a6432e5c76f4a39271d7beff
Neal JIn Oh <no4259@gmail.com>
2019-08-16 18:19:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,20 @@ def long_planeteer_calls(words)
 end
 
 
+# def long_planteer_calls(long_planteer_calls)# code an argument here
+#   # Your code here
+#   answer = false
+#   long_planteer_calls.each do |call|
+#     if call.length > 4
+#       answer = true
+#     end
+#   end
+#     answer
+# end
+
+
+
+
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
