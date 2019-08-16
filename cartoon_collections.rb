f2e0c47aba7c6c3baf0d5e00e1342e20c5f08f28
Neal JIn Oh <no4259@gmail.com>
2019-08-16 18:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,16 +28,6 @@ def long_planeteer_calls(words)
   say
 end
 
-# def long_planteer_calls(long_planteer_calls)# code an argument here
-#   # Your code here
-#   answer = false
-#   long_planteer_calls.each do |call|
-#     if call.length > 4
-#       answer = true
-#     end
-#   end
-#     answer
-# end
 
 
 def find_the_cheese# code an argument here
